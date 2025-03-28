52534f042b557765bb14c807a7dfa1cfc89eae18
Caetano <caefleury15@gmail.com>
2025-03-28 20:52:43 -0300

    adiciona multi value input in same line

---
+++ tips/multi-value-inputs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    // amount of data unknown
+    while (cin >> x) {
+        // code
+    }
+
+    int N = 10;
+    vector<int> numbers
+    // amount of data known
+    for (int i = 0; i < N ; i++){
+        cin >> numbers[i];
+    };  
+}
