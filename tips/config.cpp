055c6a35d1bbf54421d862d0870290ba402059a5
Caetano <caefleury15@gmail.com>
2025-03-29 18:20:45 -0300

    type configs

---
+++ tips/config.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define size(z) (long long)z.size()
+#define all(x) x.begin(), x.end()
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int main (){
+    ios::sync_with_stdio(false);
+
+    return 0
+}
